2a859f83ce10674b759401c767cfa5f4ec2b3f61
JaelAguilar <71282327+JaelAguilar@users.noreply.github.com>
2023-03-18 18:43:58 -0600

    Formatting of steps

--- notes/chapter2practice.py
+++ notes/chapter2practice.py
@@ -1,4 +1,4 @@
-# Loading the MNIST dataset in Keras
+# LOADING THE MNIST DATASET IN KERAS
 from keras.datasets import mnist
 
 (train_images, train_labels),(test_images, test_labels) = mnist.load_data()
@@ -10,7 +10,9 @@ train_images = train_images.astype('float32') / 255
 test_images = test_images.reshape((10000,28*28))
 test_images = test_images.astype('float32') / 255
 
-# Building the network architecture
+
+
+# BUILDING THE NETWORK ARCHITECTURE
 from keras import models
 from keras import layers
 
@@ -20,3 +22,5 @@ network = models.Sequential()
 network.add(layers.Dense(512, activation='relu',input_shape=(28*28,)))
 # W1,with softmax activation function. Output of vector of 10 probability scores
 network.add(layers.Dense(10,activation='softmax'))
+
+
